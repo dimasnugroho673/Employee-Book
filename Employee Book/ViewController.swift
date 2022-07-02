9f8ffc35726f6dd33245f87ba1e6c27508990fb3
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2022-07-03 01:07:02 +0700

    Initial Commit

---
+++ Employee Book/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employee Book
+//
+//  Created by Dimas Putro on 03/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
