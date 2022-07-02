4310305edcd3abab96ff775ad3a322bdcebfd307
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2022-07-03 02:10:12 +0700

    create project structure

--- Employee Book/Presentation/View/Login/LoginViewController.swift
+++ Employee Book/Presentation/View/Login/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Employee Book
 //
 //  Created by Dimas Putro on 03/07/22.
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
 
 
